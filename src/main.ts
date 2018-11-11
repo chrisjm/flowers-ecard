7f9b98e0eda28682c8cf00b089e3d531916a0425
Chris Mears <chris.mears@gmail.com>
2018-11-11 09:24:45 -0800

    Create card for mom ❤️

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,16 @@ import { Supplementary } from './supplementary/supplementary';
 import { InteractiveFlowers } from './animations/interactive-flowers';
 
 function main() {
-  const canvas1 = <HTMLCanvasElement>document.getElementById('supplementary');
-  const sup = new Supplementary(canvas1);
+  // const header = <HTMLCanvasElement>document.getElementById('header');
+  // const bloomingFlowers = new BloomingFlowers(header, 20);
+  // bloomingFlowers.bloom();
 
-  const canvas2 = <HTMLCanvasElement>document.getElementById('blooming-flowers');
-  const flowers1 = new BloomingFlowers(canvas2);
-  flowers1.bloom();
-
-  const canvas3 = <HTMLCanvasElement>document.getElementById('interactive-flowers');
-  const flowers2 = new InteractiveFlowers(canvas3);
+  const canvas = <HTMLCanvasElement>document.getElementById('interactive-flowers');
+  const flowers = new InteractiveFlowers(canvas);
 
   const btn = document.getElementById('clearBtn');
   btn.addEventListener('click', () => {
-    flowers2.clearCanvas();
+    flowers.clearCanvas();
   });
 }
 
